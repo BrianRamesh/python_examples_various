8ec5f2deca33afbec877cbdc324c53bbd9fea304
Brian Ramesh <brian00711@live.com>
2020-07-22 10:23:11 +0300

    Update randomnumbers.py
    added comments

--- random integers/randomnumbers.py
+++ random integers/randomnumbers.py
@@ -1,9 +1,9 @@
 
 from random import randint
-fw=open("5000_random_numbers_1_to_100.txt",'w')
+fw=open("5000_random_numbers_1_to_100.txt",'w') #create a file to store outputs
 # generate 5000 integers of 1-100
 for _ in range(5000):
-	value = randint(0, 100)
+	value = randint(0, 100)#in brackets are the minum wanted value and the maximum
 	print(value)
 	fw.write(str(value)+"\n")
 
